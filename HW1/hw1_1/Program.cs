61d7f867f9513eaf3808bc0dbce95bb0b18922bc
Alexander Syazi <alexander@syazi.ru>
2023-07-25 09:41:59 +0300

    hw1_1 worked

---
+++ HW1/hw1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+string userInputA = Console.ReadLine () ??"";
+int numberA = int.Parse(userInputA);
+
+Console.WriteLine("Введите второе число");
+string userInputB = Console.ReadLine () ??"";
+int numberB = int.Parse(userInputB);
+
+if (numberA > numberB)
+{
+    Console.Write($"{numberA}; {numberB}; max -> {numberA}");
+}
+if (numberB > numberA)
+{
+    Console.Write($"{numberA}; {numberB}; max -> {numberB}");
+}
