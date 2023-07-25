e499559a48e694a650dd15a25e5c5fa19695a433
Alexander Syazi <alexander@syazi.ru>
2023-07-25 15:36:12 +0300

    hw1_3 worked

---
+++ HW1/hw1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput); //Сокращенное преобразование в число;
+//int i = number % 2;
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} -> Делится");
+}
+else
+{
+    Console.WriteLine($"{number} Не делится");
+}
