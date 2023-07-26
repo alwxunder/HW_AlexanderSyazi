a9550729a81e557471b5980ff9a3863555dd4990
Alexander Syazi <alexander@syazi.ru>
2023-07-26 11:33:24 +0300

    hw1_4 worked

---
+++ HW1/hw1_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое число, больше нуля");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput); // ввод данных
+
+if (number <= 0) // проверка на целое и больше нуля
+{
+    Console.WriteLine("Число меньше или равно нулю");
+    return;
+}
+int i = 2; // исполнительная часть
+if (i % 2 == 0)
+    while (i <= number)
+{
+    Console.Write($"{i}, "); // вывод результата, почему получается знак процента на выводе?
+    i = i + 2;
+}
