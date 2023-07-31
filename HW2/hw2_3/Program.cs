e0955b96d4e3a2d1ada59fc03fafa5fc1211b32d
Alexander Syazi <alexander@syazi.ru>
2023-07-31 23:26:43 +0300

    add ?? null check

--- HW2/hw2_3/Program.cs
+++ HW2/hw2_3/Program.cs
@@ -35,7 +35,7 @@ else
 int Prompt(string message)
 {
     Console.Write(message);
-    string value = Console.ReadLine();
+    string value = Console.ReadLine() ??"";
     int result = Convert.ToInt32(value);
     return result;
 }
