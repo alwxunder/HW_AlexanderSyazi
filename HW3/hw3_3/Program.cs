08541fbb8188d2879101012b84d25c1f8de0cc3e
Alexander Syazi <alexander@syazi.ru>
2023-08-10 12:40:57 +0300

    add Program.cs

---
+++ HW3/hw3_3/Program.cs
@@ -0,0 +1,72 @@
+﻿//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+// -------------------------- Начало программы ----------------------------------
+#region --- 00. Configuration ---
+Console.Clear();
+Console.Title = "Задача 23. Таблица кубов чисел от 1 до N";
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+var curConsoleColor = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(@"************************************************************************
+Напишите программу, которая принимает на вход число (N) 
+    и выдаёт таблицу кубов чисел от 1 до N.
+************************************************************************");
+Console.ForegroundColor = curConsoleColor;
+#endregion
+
+#region --- 01. Input operations ---
+// Вызов метода, запрашивающего у пользователя целое число. Введённое число присваивается переменной x
+int n = GetNumberFromUser("Введите целое число: ", "Ошибка ввода данных!");
+#endregion --- 01. Input operations ---
+
+#region --- 02. Business logic ---
+int result;
+try
+{
+    // Вызов метода, возвращающего номер четверти по переданным координатам x и y.
+    // Результат вызова присваивается переменной quarter.
+    result = CounterCube(n);
+}
+catch (Exception exc)
+{
+    Console.WriteLine($"ОШИБКА! {exc.Message}");
+    return; // Завершение программы в случае ошибки
+}
+#endregion --- 02. Business logic ---
+
+#region --- 03. Output operations ---
+Console.WriteLine($"Таблица кубов от 1 до N: {result}");
+#endregion --- 03. Output operations ---
+// -------------------------- Конец программы ----------------------------------
+
+
+// -------------------------Определение методов ---------------------------------
+// Определяем функцию, выполняющую ввод целого числа с консоли
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write(message);
+            return Int32.Parse(Console.ReadLine() ?? "");
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"{errorMessage} {exc.Message}");
+        }
+    }
+}
+
+int CounterCube(int n)
+{   
+    int k = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        k = Convert.ToInt32(Math.Pow(i, 3));
+    }
+    return k;
+    
+}
+// ---------------------- Конец определения методов ----------------------------
