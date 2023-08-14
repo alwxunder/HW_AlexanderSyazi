5a303b621d760b6b0271e98c674638d6b5e83fef
Alexander Syazi <alexander@syazi.ru>
2023-08-14 22:02:56 +0500

    add Program.cs

---
+++ HW4/hw4_3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив 
+// из 8 элементов, выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+// Ввод числа с экрана
+int Prompt(string message)
+{
+    System.Console.Write(message); //Выводим приглашение ко вводу
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput); //приводим к числу
+    return result; //возвращаем результат
+}
+
+// метод для получения случайных значений массива
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length]; // обьявляем массив
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1); // заполняем случайными цифрами из диапазона SatrArr до EndArr
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($" (array[i]), "); //вывод значения массива
+    }
+    System.Console.Write($" {array[array.Length - 1]}"); //вывод значения массива
+    System.Console.WriteLine("]");
+}
+
+int length = Prompt("Длина массива: ");
+int min = Prompt("Начальное значение, для диапазона случайного числа: ");
+int max = Prompt("Конечное значение, для диапазона случайного числа: ");
+int[] array = GenerateArray(length, min, max); //заполнение массива случаными числами
+PrintArray(array); // вывод массива
