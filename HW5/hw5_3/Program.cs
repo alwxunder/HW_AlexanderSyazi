85c2810f5a9d1f5320105eee4284d6a656da09f6
Alexander Syazi <alexander@syazi.ru>
2023-08-17 02:23:25 +0500

    hw5_3 worked

---
+++ HW5/hw5_3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = GetArray(4);
+Console.WriteLine($"Random double massive : [{string.Join("|", array)}]");
+
+double maxNumber = findMaxNumber(array);
+double minNumber = findMinNumber(array);
+double differenceMaxMin = maxNumber - minNumber;
+
+Console.WriteLine($"Difference = {Math.Round((differenceMaxMin), 2)}");
+
+double[] GetArray(int size)
+{
+    double[] res = new double[size];
+
+    for (int i = 0; i < size; i++)
+    { // функция Math.Round((генерация нового вещественного массива), 2 знака)
+        res[i] = Math.Round((new Random().NextDouble()), 2);
+    }
+    return res;
+}
+
+double findMaxNumber(double[] arr)
+{
+    double maxNum = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (i > maxNum) maxNum = array[i];
+    }
+    return maxNum;
+}
+
+double findMinNumber(double[] arr)
+{
+    double minNum = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (i < minNum) minNum = array[i];
+    }
+    return minNum;
+}
