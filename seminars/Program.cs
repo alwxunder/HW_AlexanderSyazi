b9ab3b062a7c3254bbb00bd9a0f452f85d05c904
Alexander Syazi <alexander@syazi.ru>
2023-08-28 00:13:17 +0500

    add task 39

--- seminars/Program.cs
+++ seminars/Program.cs
@@ -1,4 +1,5 @@
-﻿// 31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+﻿/*
+// 31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
 // Найдите сумму отрицательных и положительных элементов массива.
 
 Console.Clear();
@@ -45,3 +46,82 @@ int GetNegativeSum(int[] arr)
     }
     return negativeSum;
 }
+*/
+
+
+// Задача 39: Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+Console.Clear();
+
+int[] array = GetArray(10, 0, 10);
+Console.WriteLine($"Random array : [{string.Join("|", array)}]");
+
+int[] reversArray = ReversArray1(array);
+Console.WriteLine($"Reverse array 1 : [{string.Join("|", array)}]");
+
+ReverseArray2(array);
+Console.WriteLine($"Reverse array 2 : [{string.Join("|", array)}]");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size]; 
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] ReversArray1(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
+
+void ReverseArray2(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArray[inArray.Length - i - 1];
+        inArray[inArray.Length - i - 1] = k;
+    }
+}
+
+/*
+// Задача 40: Напишите программу, 
+// которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+int[] array = new int[3];
+Console.WriteLine("Введите длину первой стороны", "ОШИБКА!");
+int[] array[0] = GetNumberFromUser();
+Console.WriteLine("Введите длину первой стороны", "ОШИБКА!");
+int[] array[1] = GetNumberFromUser();
+Console.WriteLine("Введите длину первой стороны", "ОШИБКА!");
+int[] array[2] = GetNumberFromUser();
+
+
+
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write(message);
+            return Int32.Parse(Console.ReadLine() ?? "");
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"{errorMessage} {exc.Message}");
+        }
+    }
+}
+*/
