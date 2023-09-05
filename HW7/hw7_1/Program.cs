975303ce09d67eb748abafa9b48fb8c1e8e335c3
Alexander Syazi <alexander@syazi.ru>
2023-09-06 02:13:03 +0500

    worked

--- HW7/hw7_1/Program.cs
+++ HW7/hw7_1/Program.cs
@@ -13,29 +13,29 @@ int rows = int.Parse(Console.ReadLine() ?? "");
 Console.Write("Введите количество столбцов массива: ");
 int columns = int.Parse(Console.ReadLine() ?? "");
 
-int[,] array = GetArray(rows, columns, 0, 10);
+double[,] array = GetArray(rows, columns);
 PrintArray(array);
 
-int[,] GetArray(int m, int n, int minValue, int maxValue)
+double[,] GetArray(int m, int n)
 {
-    int[,] result = new int[m, n];
+    double[,] result = new double[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().Next(minValue, maxValue + 1);
+            result[i, j] = new Random().NextDouble() * 10;
         }
     }
     return result;
 }
 
-void PrintArray(int[,] inArray)
+void PrintArray(double[,] inArray)
 {
     for (int i = 0; i < inArray.GetLength(0); i++)
     {
         for (int j = 0; j < inArray.GetLength(1); j++)
         {
-            Console.Write($"{inArray[i, j]} ");
+            Console.Write($"{inArray[i, j]:F2} ");
         }
         Console.WriteLine();
     }
